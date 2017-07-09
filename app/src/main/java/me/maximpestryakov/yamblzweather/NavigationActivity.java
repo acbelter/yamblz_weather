d6842d4433f687a61386524d5a66dd8793b63395
Maxim Pestryakov <maximpestryakov@gmail.com>
2017-07-09 22:11:11 +0300

    Added 3 fragments

--- app/src/main/java/me/maximpestryakov/yamblzweather/NavigationActivity.java
+++ app/src/main/java/me/maximpestryakov/yamblzweather/NavigationActivity.java
@@ -3,6 +3,7 @@ package me.maximpestryakov.yamblzweather;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -39,6 +40,8 @@ public class NavigationActivity extends AppCompatActivity
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_weather);
+        replaceFragment(WeatherFragment.newInstance());
     }
 
     @Override
@@ -52,15 +55,26 @@ public class NavigationActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.nav_weather:
+                replaceFragment(WeatherFragment.newInstance());
+                break;
+            case R.id.nav_settings:
+                replaceFragment(SettingsFragment.newInstance());
+                break;
 
-        if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_about) {
-
+            case R.id.nav_about:
+                replaceFragment(AboutFragment.newInstance());
+                break;
         }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
